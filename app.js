b86ecf4d0f347f3025eedc311c228693c4909689
dev3r3nze <dev3r3nze@gmail.com>
2023-03-03 12:31:37 +0100

    todo perfe

--- app.js
+++ app.js
@@ -1,46 +1,74 @@
-var arrVentas = []
-var arrNums = []
+var arrVentas = [[],[],[],[],[]];
+var arrVentasIndividuales = [[],[],[],[],[]];
+var arrNums = [[],[],[],[],[]]
 var ventasTotales = 0
-
+var campeonatoText = "Campeonato 10:00"
+var campeonato = 1;
+var lastNum = 0
 
 function Numerar(num){
     if(num!= Number){
         num = Number(num)
     }
-    arrNums.push(num)
+    arrNums[campeonato-1].push(num)
     document.getElementById("numSelect").innerHTML = "<p>"+num+"</p>"
+    lastNum = num
 }
 
 function Confirmar(){
     ventasTotales = 0
-    arrVentas.push(arrNums[arrNums.length-1])
-    CrearRegistro(arrNums[arrNums.length-1])
-
+    arrVentasIndividuales[campeonato-1] = 0
+    arrVentas[campeonato-1].push(arrNums[campeonato-1][arrNums[campeonato-1].length-1])
+    CrearRegistro(arrNums[campeonato-1][arrNums[campeonato-1].length-1])
     ActualizarStats()
 }
 function Borrar(){
     ventasTotales = 0
-    arrVentas.push(-arrNums[arrNums.length-1])
-    CrearRegistro(-arrNums[arrNums.length-1])
+    arrVentasIndividuales[campeonato-1] = 0
+    arrVentas[campeonato-1].push(-arrNums[campeonato-1][arrNums[campeonato-1].length-1])
+    CrearRegistro(-arrNums[campeonato-1][arrNums[campeonato-1].length-1])
 
     ActualizarStats()
 }
 function ActualizarStats(){
-    for(var i = 0; i < arrVentas.length;i++){
-        ventasTotales += arrVentas[i]
+    for(var i = 0; i < arrVentas[campeonato-1].length;i++){
+        arrVentasIndividuales[campeonato-1] += arrVentas[campeonato-1][i]
+
     }
 
-    document.getElementById("vendidoStat").innerHTML = "<p> Vendido: "+ventasTotales+" entradas </p>"
-    document.getElementById("recaudadoStat").innerHTML = "<p> Recaudado: "+ventasTotales*5+" €</p>"
+    arrVentas.forEach(subArr => {
+        subArr.forEach(valor => {
+            ventasTotales += valor;
+        });
+    });
+    
+    console.log(ventasTotales);
+
+    ActualizarSoloStats();
+}
+function ActualizarSoloStats(){
+    document.getElementById("vendidoStat").innerHTML = "<p> Vendido: "+arrVentasIndividuales[campeonato-1]+" entradas </p>"
+    document.getElementById("recaudadoStat").innerHTML = "<p> Recaudado: "+arrVentasIndividuales[campeonato-1]*5+" €</p>"
+    document.getElementById("ventaTotalStat").innerHTML="<p>Total vendido: "+ventasTotales+" ("+ventasTotales*5+" €)</p>"
+
 }
 
 function CrearRegistro(num){
-    if(num>0)numText = document.createTextNode("+"+num)
-    else(numText = document.createTextNode(num))
+    if(num>0)numText = document.createTextNode("+"+num + " "+ campeonatoText)
+    else(numText = document.createTextNode(num + " "+ campeonatoText))
     node = document.createElement("li")
     node.appendChild(numText)
     var parent = document.getElementById('registroUl');
     var newChild = node
     parent.insertBefore(newChild, parent.firstChild);
 
+}
+
+function CambioCampeonato(camp){
+    campeonato = camp
+    campeonatoText = document.getElementsByClassName("campeonatoText")[document.getElementsByClassName("campeonatoText").length - campeonato].textContent;
+    document.getElementById("titleDiv").innerHTML="<p>"+campeonatoText+"</p>"
+
+    arrNums[campeonato-1].push(lastNum)
+    ActualizarSoloStats()
 }
